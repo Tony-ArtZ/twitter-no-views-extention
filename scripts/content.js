5b90c93b6b45915075cf41b71715b39d0a443f9a
Tony-ArtZ <adarshgku@gmail.com>
2023-01-12 16:36:15 +0530

    fixed post views

--- scripts/content.js
+++ scripts/content.js
@@ -4,8 +4,7 @@ chrome.storage.sync.get(["activated"]).then((result) => {
 
 const getContent = () => {
   let tweetStats = document.querySelectorAll(
-    ".css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws, .css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co"
-  );
+    ".css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws, .css-901oao.css-16my406.r-1nao33i.r-poiln3.r-bcqeeo.r-qvutc0")
   window.location.href == "https://twitter.com/home";
 
   tweetStats?.forEach((element) => {
