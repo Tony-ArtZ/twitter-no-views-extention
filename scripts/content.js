20f4f3ccf0b4c1f7cdb611406e4a618d611f898a
Tony-ArtZ <adarshgku@gmail.com>
2023-01-20 14:55:09 +0530

    Fixed it again

--- scripts/content.js
+++ scripts/content.js
@@ -4,10 +4,11 @@ chrome.storage.sync.get(["activated"]).then((result) => {
 
 const getContent = () => {
   let tweetStats = document.querySelectorAll(
-    ".css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws, .css-901oao.css-16my406.r-1nao33i.r-poiln3.r-bcqeeo.r-qvutc0")
+    ".css-4rbku5.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1777fci.r-bt1l66.r-1ny4l3l.r-bztko3.r-lrvibr, .css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2 :nth-child(3) :nth-child(1)")
   window.location.href == "https://twitter.com/home";
 
   tweetStats?.forEach((element) => {
-    element.firstChild.style.display = "none";
+    console.log(element.children.length)
+    element.parentElement.remove()
   });
 };
